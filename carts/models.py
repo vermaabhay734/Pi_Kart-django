ff8f7d609e75a669c3b55425b62f4faf7920cf34
abhay <vermaabhay734@gmail.com>
2021-07-17 14:21:51 +0530

    Add variation feature in cart

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from store.models import Product
+from store.models import Product, Variation
 # from accounts.models import Account
 
 
@@ -16,16 +16,14 @@ class Cart(models.Model):
 class CartItem(models.Model):
     # user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     variations = models.ManyToManyField(Variation, blank=True)
+    variations = models.ManyToManyField(Variation, blank=True)
     cart    = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.product
 
     def sub_total(self):
         return self.product.price * self.quantity
 
-#     def __unicode__(self):
-#         return self.product
+    def __unicode__(self):
+        return self.product
--- store/models.py
+++ store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from category.models import Category
 from django.urls import reverse
 # from accounts.models import Account
-# from django.db.models import Avg, Count
+from django.db.models import Avg, Count
 
 # Create your models here.
 
@@ -38,29 +38,29 @@ class Product(models.Model):
 #             count = int(reviews['count'])
 #         return count
 
-# class VariationManager(models.Manager):
-#     def colors(self):
-#         return super(VariationManager, self).filter(variation_category='color', is_active=True)
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
 
-#     def sizes(self):
-#         return super(VariationManager, self).filter(variation_category='size', is_active=True)
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
 
-# variation_category_choice = (
-#     ('color', 'color'),
-#     ('size', 'size'),
-# )
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
 
-# class Variation(models.Model):
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     variation_category = models.CharField(max_length=100, choices=variation_category_choice)
-#     variation_value     = models.CharField(max_length=100)
-#     is_active           = models.BooleanField(default=True)
-#     created_date        = models.DateTimeField(auto_now=True)
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value     = models.CharField(max_length=100)
+    is_active           = models.BooleanField(default=True)
+    created_date        = models.DateTimeField(auto_now=True)
 
-#     objects = VariationManager()
+    objects = VariationManager()
 
-#     def __str__(self):
-#         return self.variation_value
+    def __str__(self):
+        return self.variation_value
 
 
 # class ReviewRating(models.Model):
