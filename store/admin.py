18b73ad66e3a114a5831c74ac8935118dabb11a4
vermaabhay734 <vermaabhay734@gmail.com>
2021-08-04 14:44:35 +0530

    Add product gallery

--- store/admin.py
+++ store/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-from .models import Product,Variation, ReviewRating
-# import admin_thumbnails
+from .models import Product,Variation, ReviewRating, ProductGallery
+import admin_thumbnails
 
-# @admin_thumbnails.thumbnail('image')
-# class ProductGalleryInline(admin.TabularInline):
-#     model = ProductGallery
-#     extra = 1
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
     prepopulated_fields = {'slug': ('product_name',)}
-    # inlines = [ProductGalleryInline]
+    inlines = [ProductGalleryInline]
 
 class VariationAdmin(admin.ModelAdmin):
     list_display = ('product', 'variation_category', 'variation_value', 'is_active')
@@ -20,4 +20,4 @@ class VariationAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
 admin.site.register(ReviewRating)
-# admin.site.register(ProductGallery)
+admin.site.register(ProductGallery)
