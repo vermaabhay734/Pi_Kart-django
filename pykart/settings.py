51d3b93d64bcecb192242b842da6a31df8f31223
vermaabhay734 <vermaabhay734@gmail.com>
2021-08-12 20:14:00 +0530

    Automatically logout after inactivity

--- pykart/settings.py
+++ pykart/settings.py
@@ -54,7 +54,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
+
+SESSION_EXPIRE_SECONDS = 3600  # 1 hour
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = 'accounts/login/'
 
 ROOT_URLCONF = 'pykart.urls'
 
